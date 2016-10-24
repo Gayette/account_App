40f9d0bcf8e711eeba06bb081e766f3414ecb3a9
Gayette <gaelventurin@hotmail.fr>
2016-10-24 17:26:42 +0200

    filtre angular

---
+++ js/addusers.js
@@ -0,0 +1,21 @@
+
+
+var modal = document.getElementById('addModal');
+
+var btn = document.getElementsByClassName("add");
+
+var span = document.getElementsByClassName("closeModal")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
-console.log("app.js");
 
 var app = angular.module('app', []);
 
-	app.controller('usersCtrl', function($scope, $http) {
-		$http({
-			method: "GET",
-			url: "json/users.json"
-		}).then(function (response) {
-				$scope.usersData = response.data.records;
-	} ,
+app.controller('usersCtrl', function($scope, $http) {
+    $http({
+        method:'GET',
+        url: 'json/users.json'
 
-        function errorCallback(response) {
-     console.log("Failure");
-     console.error(response);		
-  });
+    }).then(function (response) {
+        $scope.usersData = response.data.records;
+
+        $scope.tablUsers = [];
+        for ($user in $scope.usersData) {
+            $scope.tablUsers[$scope.usersData[$user].Id]
+             = $scope.usersData[$user].username;
+        }
+    },
+        function errorCallback(response){
+        console.log("Failure");
+        console.error(response);
+    });
 });
 
 
 
+app.controller('depenseCtrl', function($scope, $http) {
+    $http({
+        method:'GET',
+        url: 'json/depenses.json'
 
+    }).then(function (response) {
+        $scope.depenseData = response.data.records;
+     
+    },
+        function errorCallback(response){
+        console.log("Failure");
+        console.error(response);
+    });
+});
